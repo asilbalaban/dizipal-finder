8f4e8d86defe536c0fc8fb82acc9f051ce1a869e
Asil Balaban <uyelik@asilbalaban.com>
2022-02-18 09:24:14 +0300

    en son kaldığı yeri hatırlama özelliği eklendi

--- dizipal-finder.py
+++ dizipal-finder.py
@@ -1,3 +1,4 @@
+import re
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,7 +6,6 @@ def send_request(url):
     try:
         reqs = requests.get(url)
         soup = BeautifulSoup(reqs.text, 'html.parser')
-
         for title in soup.find_all('title'):
             # print(url + " internet sitesi başlığı: " + title.get_text())
             if(title.get_text() == 'DiziPAL – yabancı dizi, film ve anime izle'):
@@ -15,19 +15,26 @@ def send_request(url):
     except:
         pass
 
-
 def create_readme(url):
     f = open("README.md", "w")
     f.write("# dizipal-finder\nher seferinde aramak istemeyenler için.\n\nson bulunan site: " + str(url))
     f.close()
 
-number = 162
-while(number < 1000):
-    url = "http://www.dizipal" + str(number) + ".com/"
-    website = send_request(url)
-    if(website != None):
-        print(website)
-        create_readme(url)
-        exit()
-    else:
-        number += 1
+def get_last_url_digit():
+    f = open("README.md", "r")
+    x = re.findall(r"[\d]{1,4}", f.read())
+    return x[0]
+
+def main():
+    digit = int(get_last_url_digit())
+    while(digit < (digit + 100)):
+        url = "http://www.dizipal" + str(digit) + ".com/"
+        website = send_request(url)
+        if(website != None):
+            print(website)
+            create_readme(url)
+            exit()
+        else:
+            digit += 1
+
+main()
