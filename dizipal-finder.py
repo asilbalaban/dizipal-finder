fcf3ca35b1907fb09da15196bfc26b9347a2a5de
Asil Balaban <uyelik@asilbalaban.com>
2022-03-28 17:13:03 +0300

    Update dizipal-finder.py

--- dizipal-finder.py
+++ dizipal-finder.py
@@ -1,5 +1,4 @@
-import re
-import requests
+import re, os, requests, sys
 from bs4 import BeautifulSoup
 
 def send_request(url):
@@ -7,7 +6,7 @@ def send_request(url):
         reqs = requests.get(url)
         soup = BeautifulSoup(reqs.text, 'html.parser')
         for title in soup.find_all('title'):
-            # print(url + " internet sitesi başlığı: " + title.get_text())
+            print(url + " internet sitesi başlığı: " + title.get_text())
             if(title.get_text() == 'DiziPAL – yabancı dizi, film ve anime izle'):
                 return url
             else:
@@ -16,25 +15,43 @@ def send_request(url):
         pass
 
 def create_readme(url):
-    f = open("README.md", "w")
+    f = open(get_current_path("README.md"), "w")
     f.write("# dizipal-finder\nher seferinde aramak istemeyenler için.\n\ndizipal güncel adres: " + str(url))
     f.close()
 
 def get_last_url_digit():
-    f = open("README.md", "r")
+    f = open(get_current_path("README.md"), "r")
     x = re.findall(r"[\d]{1,4}", f.read())
     return x[0]
 
 def main():
     digit = int(get_last_url_digit())
-    while(digit < (digit + 100)):
+    while(True):
         url = "http://www.dizipal" + str(digit) + ".com/"
+        print(url)
         website = send_request(url)
         if(website != None):
-            print(website)
-            create_readme(url)
-            exit()
+            isItOk = input('Bu sayfayı yüklemek ister misiniz? (e/h)')
+            if(isItOk == 'e'):
+                print(website)
+                create_readme(url)
+                commit()
+                exit()
+            else:
+                digit += 1
         else:
             digit += 1
 
-main()
+def commit():
+    os.system("git add .")
+    os.system("git commit -m 'auto commit by dizipal-finder.py'")
+    os.system("git push")
+
+def get_current_path(filename = None):
+    if(filename == None):
+        return os.path.dirname(os.path.realpath(__file__))
+    else:
+        return os.path.dirname(os.path.realpath(__file__)) + "/" + filename
+
+if __name__ == "__main__":
+    main()
